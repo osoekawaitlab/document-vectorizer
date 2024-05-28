5cc601e3ee9ba45e77cd4d433cc0f3f0bd347a8f
osoken <osoken@osoekawalab.com>
2024-05-28 18:10:48 +0900

    feat(document_vectorizer): add NomicEmbedTextDocumentVectorizerCore creation

--- oldv/document_vectorizers/factory.py
+++ oldv/document_vectorizers/factory.py
@@ -1,12 +1,16 @@
 from ..settings import (
     AllMiniLmDocumentVectorizerCoreSettings,
     DocumentVectorizerCoreSettings,
+    NomicEmbedTextDocumentVectorizerCoreSettings,
 )
 from .all_minilm import AllMiniLmVectorizerCore
 from .base import BaseDocumentVectorizerCore
+from .nomic_embed_text import NomicEmbedTextVectorizerCore
 
 
 def create_document_vectorizer_core(settings: DocumentVectorizerCoreSettings) -> BaseDocumentVectorizerCore:
     if isinstance(settings, AllMiniLmDocumentVectorizerCoreSettings):
         return AllMiniLmVectorizerCore.create()
+    elif isinstance(settings, NomicEmbedTextDocumentVectorizerCoreSettings):
+        return NomicEmbedTextVectorizerCore.create()
     raise NotImplementedError()
--- tests/document_vectorizers/test_factory.py
+++ tests/document_vectorizers/test_factory.py
@@ -1,7 +1,10 @@
 from pytest_mock import MockerFixture
 
 from oldv.document_vectorizers.factory import create_document_vectorizer_core
-from oldv.settings import AllMiniLmDocumentVectorizerCoreSettings
+from oldv.settings import (
+    AllMiniLmDocumentVectorizerCoreSettings,
+    NomicEmbedTextDocumentVectorizerCoreSettings,
+)
 
 
 def test_create_all_minilm_document_vectorizer_core(mocker: MockerFixture) -> None:
@@ -9,3 +12,10 @@ def test_create_all_minilm_document_vectorizer_core(mocker: MockerFixture) -> No
     actual = create_document_vectorizer_core(settings=AllMiniLmDocumentVectorizerCoreSettings())
     AllMiniLmVectorizerCore.create.assert_called_once_with()
     assert actual == AllMiniLmVectorizerCore.create.return_value
+
+
+def test_create_nomic_embed_text_document_vectorizer_core(mocker: MockerFixture) -> None:
+    NomicEmbedTextVectorizerCore = mocker.patch("oldv.document_vectorizers.factory.NomicEmbedTextVectorizerCore")
+    actual = create_document_vectorizer_core(settings=NomicEmbedTextDocumentVectorizerCoreSettings())
+    NomicEmbedTextVectorizerCore.create.assert_called_once_with()
+    assert actual == NomicEmbedTextVectorizerCore.create.return_value
