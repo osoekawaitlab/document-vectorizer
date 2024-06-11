37caad7c903be4e00ac77b85b870b7334e77451c
osoken <osoken@osoekawalab.com>
2024-06-11 21:56:44 +0900

    feat(cli): support batch-vectorize

--- oldv/interfaces/cli.py
+++ oldv/interfaces/cli.py
@@ -11,11 +11,25 @@ def main() -> None:
     vectorize_parser = subcommand_parser.add_parser("vectorize")
     vectorize_parser.add_argument("text")
     vectorize_parser.add_argument("output_file")
+    batch_vectorize_parser = subcommand_parser.add_parser("batch-vectorize")
+    batch_vectorize_parser.add_argument("input_file")
+    batch_vectorize_parser.add_argument("output_file")
     args = parser.parse_args()
     if args.subcommand == "vectorize":
         settings = DocumentVectorizerAppSettings()
         app = DocumentVectorizerApp.create(settings=settings)
         document_vector = app.vectorize(doc=Document(content=args.text))
         output_file = args.output_file
-        with open(output_file, "w") as f:
+        with open(output_file, "w", encoding="utf-8") as f:
             f.write(document_vector.model_dump_json())
+    elif args.subcommand == "batch-vectorize":
+        settings = DocumentVectorizerAppSettings()
+        app = DocumentVectorizerApp.create(settings=settings)
+        with open(args.input_file, "r", encoding="utf-8") as f:
+            documents = [Document.model_validate_json(line) for line in f]
+        document_vectors = app.batch_vectorize(docs=documents)
+        output_file = args.output_file
+        with open(output_file, "w", encoding="utf-8") as f:
+            for document_vector in document_vectors:
+                f.write(document_vector.model_dump_json())
+                f.write("\n")
--- tests/test_integration_cli.py
+++ tests/test_integration_cli.py
@@ -3,7 +3,10 @@ import os
 import subprocess
 import tempfile
 
-from .document_vectorizers.fixtures import all_minilm_hello_world_vector
+from .document_vectorizers.fixtures import (
+    all_minilm_hello_computer_vector,
+    all_minilm_hello_world_vector,
+)
 
 
 def test_cli_run_single_document_json_file_output(patch_all_minilm_environment_variable: None) -> None:
@@ -23,3 +26,32 @@ def test_cli_run_single_document_json_file_output(patch_all_minilm_environment_v
             data = json.load(output_file)
         assert "vector" in data
         assert data["vector"] == all_minilm_hello_world_vector.tolist()
+
+
+def test_cli_run_multiple_document_jsonl_file_output(patch_all_minilm_environment_variable: None) -> None:
+    with tempfile.TemporaryDirectory() as tempdir:
+        input_file_name = "input.jsonl"
+        with open(os.path.join(tempdir, input_file_name), "w") as input_file:
+            json.dump({"content": "Hello World"}, input_file)
+            input_file.write("\n")
+            json.dump({"content": "Hello Computer"}, input_file)
+            input_file.write("\n")
+        output_file_name = "output.jsonl"
+        error_code = subprocess.run(
+            [
+                "oldv",
+                "batch-vectorize",
+                os.path.join(tempdir, input_file_name),
+                os.path.join(tempdir, output_file_name),
+            ],
+            stdout=subprocess.PIPE,
+            env=os.environ,
+        )
+        assert error_code.returncode == 0
+        with open(os.path.join(tempdir, output_file_name), "r") as output_file:
+            data = [json.loads(line) for line in output_file]
+        assert len(data) == 2
+        assert "vector" in data[0]
+        assert data[0]["vector"] == all_minilm_hello_world_vector.tolist()
+        assert "vector" in data[1]
+        assert data[1]["vector"] == all_minilm_hello_computer_vector.tolist()
